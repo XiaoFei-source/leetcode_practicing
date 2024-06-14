9ddb1c0c2f267323ae4cebca0066dd03f2e74f0f
sa1m0n <bill90456bill@gmail.com>
2024-06-14 21:25:56 +0800

    code_review

--- sorting_practice/2_insertion_sort.c
+++ sorting_practice/2_insertion_sort.c
@@ -14,7 +14,7 @@ void isertion_sort(int arr[], int n)
 {
     for (int i = 0; i < n ; i++){
         int j = i;
-        while (j >0 && arr[j - 1] > arr[j]){
+        while (j > 0 && arr[j - 1] > arr[j]){
             int temp = arr[j];
             arr[j] = arr[j-1];
             arr[j-1] = arr[j];
