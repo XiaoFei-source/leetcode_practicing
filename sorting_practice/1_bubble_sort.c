f3823a336fb7cc8ac4f547857c5e6c113c40e112
sa1m0n <bill90456bill@gmail.com>
2024-06-14 21:28:38 +0800

    code_review

--- sorting_practice/1_bubble_sort.c
+++ sorting_practice/1_bubble_sort.c
@@ -8,11 +8,11 @@
 //平均情況： O(n^2)
 
 void bubble_sort(int arr[], int n)
-{
-    for(int i =0;i < n - 1; i++){
-        for(int j = 0;j <n - i - i; ++j){
+{   int i , j, temp;
+    for(i =0;i < n - 1; i++){
+        for(j = 0;j <n - i - i; ++j){
             if(arr[j] > arr[j+1]){
-            int temp = arr[j];
+            temp = arr[j];
             arr[j] = arr[j+1];
             arr[j+1] = temp;
             } 
